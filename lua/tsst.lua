a5482ffc29b3f89a7f66e4fad535254ee66b25ad
Kafva <kafva.one@gmail.com>
2025-03-08 09:07:50 +0100

    Improve diff per character

--- lua/tsst.lua
+++ lua/tsst.lua
@@ -23,6 +23,69 @@ local function failed_message(msg)
         .. '\n'
 end
 
+---@param filepath string
+---@return string
+local function readfile(filepath)
+    local content
+    local fd, err
+    fd, err = vim.uv.fs_open(filepath, 'r', 438)
+
+    if not fd then
+        error(err or ('Failed to open ' .. filepath))
+    end
+
+    content, err = vim.uv.fs_read(fd, 8192)
+
+    if not content then
+        error(err or ('Failed to read ' .. filepath))
+    end
+
+    _, err = vim.uv.fs_close(fd)
+    if err then
+        error(err)
+    end
+
+    return content
+end
+
+---@param toprint string
+---@param tocompare string
+---@return string
+local function string_color_diff(toprint, tocompare)
+    local msg = ''
+    for i = 1, #tocompare do
+        if i > #toprint then
+            -- rhs string is longer than lhs
+            msg = msg .. ANSI_RED_BG .. " " .. ANSI_RESET
+            goto continue
+        end
+
+        local char = toprint:sub(i, i)
+        if char ~= tocompare:sub(i, i) then
+            if char == ' ' then
+                msg = msg .. ANSI_RED_BG .. char .. ANSI_RESET
+            else
+                msg = msg .. ANSI_RED .. char .. ANSI_RESET
+            end
+        else
+            msg = msg .. char
+        end
+        ::continue::
+    end
+
+    return msg
+end
+
+---@param expected any
+---@param actual any
+---@return string
+local function getdiff(expected, actual)
+    local expected_s = tostring(expected)
+    local actual_s = tostring(actual)
+    return "Expected:  " .. string_color_diff(expected_s, actual_s) .. "\n" ..
+           "Actual:    " .. string_color_diff(actual_s, expected_s) .. "\n"
+end
+
 ---@return boolean
 function M.run_test(testfile)
     local modpath = testfile:gsub('/', '.'):gsub('.lua$', '')
@@ -65,51 +128,6 @@ end, { nargs = 1 })
 
 -- Test utilities --------------------------------------------------------------
 
----@param filepath string
----@return string
-local function readfile(filepath)
-    local content
-    local fd, err
-    fd, err = vim.uv.fs_open(filepath, 'r', 438)
-
-    if not fd then
-        error(err or ('Failed to open ' .. filepath))
-    end
-
-    content, err = vim.uv.fs_read(fd, 8192)
-
-    if not content then
-        error(err or ('Failed to read ' .. filepath))
-    end
-
-    _, err = vim.uv.fs_close(fd)
-    if err then
-        error(err)
-    end
-
-    return content
-end
-
----@param expected any
----@param actual any
----@return string
-local function colordiff(expected, actual)
-    local expected_s = tostring(expected)
-    local actual_s = tostring(actual)
-    msg = "Expected:\n  " .. expected_s .. "\n"
-    msg = msg .. "Actual:\n  "
-    for i = 1, #expected_s do
-        if i > #actual_s then
-            msg = msg .. ANSI_RED_BG .. " " .. ANSI_RESET
-        elseif expected_s:sub(i, i) ~= actual_s:sub(i, i) then
-            msg = msg .. ANSI_RED .. actual_s:sub(i,i) .. ANSI_RESET
-        else
-            msg = msg .. actual_s:sub(i, i)
-        end
-    end
-    return msg .. "\n"
-end
-
 function M.rm_f(filepath)
     local _, err, errno = vim.uv.fs_unlink(filepath)
     if errno ~= nil and errno ~= 'ENOENT' then
@@ -124,7 +142,7 @@ function M.assert_eql(expected, actual)
         return
     end
     local msg = failed_message()
-    msg = msg .. colordiff(expected, actual)
+    msg = msg .. getdiff(expected, actual)
     error(msg)
 end
 
@@ -135,7 +153,7 @@ function M.assert_eql_tables(expected, actual)
         if expected[i] ~= actual[i] then
             local msg = failed_message()
             msg = msg .. string.format('Difference at index %d\n', i)
-            msg = msg .. colordiff(expected[i], actual[i])
+            msg = msg .. getdiff(expected[i], actual[i])
             error(msg)
         end
     end
@@ -149,7 +167,7 @@ function M.assert_eql_file(expected_file, actual)
         if expected[i] ~= actual[i] then
             local msg = failed_message()
             msg = msg .. string.format('Difference at %s:%d\n', expected_file, i)
-            msg = msg .. colordiff(expected[i], actual[i])
+            msg = msg .. getdiff(expected[i], actual[i])
             error(msg)
         end
     end
